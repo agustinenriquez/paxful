731fcf14aac4d95bed9bdd8c68a0265d60f54c41
m0tz <agustin_enriquez@hotmail.com>
2020-07-28 19:25:16 -0300

    bye web

--- web/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from web.views import UserCreateView, WalletCreateView, TransferCreateView, WalletListView, post_users
-
-urlpatterns = [
-    path("", UserCreateView.as_view(), name="index"),
-    path("wallet/add/", WalletCreateView.as_view(), name="create-wallet"),
-    path("wallet/all", WalletListView.as_view(), name="list-wallet"),
-    path("wallet/<int:pk>/transfer/", TransferCreateView.as_view(), name="wallet-transfer"),
-]
