f06036d5d91bf0074852a19f05f79974dd305ed4
m0tz <agustin_enriquez@hotmail.com>
2020-07-29 14:32:39 -0300

    factories

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,48 @@
-from django.test import TestCase
+from factory import django
+from api.models import Platform, Transaction
+import factory
+from decimal import Decimal
+import uuid
+import random
+import string
 
-# Create your tests here.
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = "auth.User"
+        django_get_or_create = ("username",)
+
+    username = "agustest"
+    password = factory.PostGenerationMethodCall("set_password", "eduzen!")
+    email = "agustest@gmail.com"
+
+
+class WalletFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = "api.Wallet"
+        django_get_or_create = ("address",)
+
+    user = UserFactory.create()
+    balance = Decimal("1.0")
+    alias = "testwallet"
+    address = uuid.uuid4()
+
+
+class TransactionFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = "api.Transaction"
+        django_get_or_create = ("code",)
+
+    origin_address = WalletFactory.create().address
+    destination_address = WalletFactory.create().address
+    amount = Decimal("0.5")
+    code = "".join(random.choice(string.ascii_uppercase + string.digits) for _ in range(16))
+
+
+class PlatformFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = "api.Platform"
+        django_get_or_create = ("name",)
+
+    name = "paxful"
+    profit = Decimal("0")
