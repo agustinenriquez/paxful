bbec4000c742f4bac27e40d992c75de0a06298ec
m0tz <agustin_enriquez@hotmail.com>
2020-07-30 11:17:17 -0300

    api docs

--- paxful/urls.py
+++ paxful/urls.py
@@ -17,13 +17,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from api.views import CustomAuthToken
-
+from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     path("", include("api.urls")),
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
     path("api-token-auth/", CustomAuthToken.as_view()),
+    path("docs/", include_docs_urls(title="Paxful API Documentation", public=False)),
 ]
 
 if settings.DEBUG:
